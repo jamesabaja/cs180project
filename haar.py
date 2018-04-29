089db034df2f9c200f881869dcf186b1e5efee8b
James Abaja <jgabaja@gmail.com>
2018-04-29 14:51:17 +0800

    added initial files for HC cropping

---
+++ haar.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2 as cv
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+#eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+img = cv.imread('sample.jpg')
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+cv.imshow('img',roi_color)
+cv.waitKey(0)
+cv.destroyAllWindows()
