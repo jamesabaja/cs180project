a2ca62ba1190d18834527bd8a3b1084407ee3274
James Abaja <jgabaja@gmail.com>
2018-05-11 11:28:54 +0800

    added indo pics in 180 folder

--- haar.py
+++ haar.py
@@ -2,13 +2,17 @@ import numpy as np
 import cv2 as cv
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 #eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
-img = cv.imread('sample.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-cv.imshow('img',roi_color)
+for i in range(1, 10):
+	img = cv.imread('sample' + str(i) + '.jpg')
+	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	j = 1
+	for (x,y,w,h) in faces:
+	    cv.rectangle(img,(x,y),(x+w,y+h),(0,0,0),2)
+	    roi_gray = gray[y:y+h, x:x+w]
+	    roi_color = img[y:y+h, x:x+w]
+	    cv.imwrite('preprocessed/img'+str(j)+'.jpg', roi_color)
+	    j += 1
+'''cv.imshow('img',roi_color)
 cv.waitKey(0)
-cv.destroyAllWindows()
+cv.destroyAllWindows()'''
